7c6b57becfe08e77d01bc9a4dd4488de1b7a47f2
timertimertimer <timerkhan2002@gmail.com>
2025-03-15 05:44:12 +0300

    v1.0.11

--- web3db/models/base.py
+++ web3db/models/base.py
@@ -10,17 +10,11 @@ class Base(DeclarativeBase):
 
 class BaseModel:
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=True)
-    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow,
-                                                 nullable=True)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=True
+    )
 
 
 class SocialBaseModel(BaseModel):
-    password_updated_at: Mapped[datetime] = mapped_column(DateTime, onupdate=datetime.utcnow, nullable=True)
+    password_updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
     password: Mapped[str]
-
-
-@event.listens_for(SocialBaseModel, 'before_update')
-def update_password_timestamp(mapper, connection, target):
-    if target.password != target.__original_password:
-        target.password_updated_at = datetime.utcnow()
-        target.__original_password = target.password
